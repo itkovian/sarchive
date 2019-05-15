0d4bcbee415428cd6595d1f3a69f0d2d0ac5f094
Andy Georges <itkovian@gmail.com>
2019-05-15 22:47:39 +0200

    Spool argument

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+extern crate clap;
+extern crate inotify;
+
+use clap::{Arg, App};
+
+
 fn main() {
-    println!("Hello, world!");
+
+    let matches = App::new("SArchive")
+        .version("0.1.0")
+        .author("Andy Georges <itkovian+sarchive@gmail.com>")
+        .about("Archive slurm user job scripts")
+        .arg(Arg::with_name("spool")
+            .long("spool")
+            .short("s")
+            .help("Location of the Slurm StateSaveLocation (where the job hash dirs are kept)")
+        )
+        .get_matches();
+
 }
