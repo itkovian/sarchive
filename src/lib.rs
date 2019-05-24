ff02d36de920e91ef7dafb986afddbb79b5536e9
Andy Georges <itkovian@gmail.com>
2019-05-25 00:03:31 +0200

    Wait for dir creation and loop checking for file existence

--- src/lib.rs
+++ src/lib.rs
@@ -31,6 +31,7 @@ use std::io::Error;
 use std::path::{Path, PathBuf};
 use std::sync::mpsc::channel;
 use std::time::Duration;
+use std::thread::sleep;
 
 /// Representation of an entry in the Slurm job spool hash directories
 pub struct SlurmJobEntry {
@@ -38,16 +39,13 @@ pub struct SlurmJobEntry {
     path: PathBuf,
     /// The job ID
     jobid: String,
-    /// The filename in the job directory, this will be a suffix of the archived filename
-    filename: String,
 }
 
 impl SlurmJobEntry {
-    fn new(p: &PathBuf, id: &str, f: &str) -> SlurmJobEntry {
+    fn new(p: &PathBuf, id: &str) -> SlurmJobEntry {
         SlurmJobEntry {
             path: p.clone(),
             jobid: id.to_string(),
-            filename: f.to_string()
         }
     } 
 }
@@ -64,29 +62,50 @@ impl SlurmJobEntry {
 /// We return a tuple of two strings: the job ID and the filename, wrapped in
 /// an Option.
 fn is_job_path(path: &Path) -> Option<(&str, &str)> {
-    if path.is_file() {
-        let file = path.file_name().unwrap().to_str().unwrap();
-        let parent = path.parent().unwrap();
-        let jobname = parent.file_name().unwrap().to_str().unwrap();
+    if path.is_dir() {
+        let dirname = path.file_name().unwrap().to_str().unwrap();
 
-        if jobname.starts_with("job.") {
-            return Some((parent.extension().unwrap().to_str().unwrap(), file));
+        if dirname.starts_with("job.") {
+            return Some((path.extension().unwrap().to_str().unwrap(), dirname));
         };
     }
     debug!("{:?} is not a considered job path", &path);
     None
 }
 
-/// Archives the file from the given SlurmJobEntry's path. 
+/// Archives the files from the given SlurmJobEntry's path. 
 fn archive(archive_path: &Path, slurm_job_entry: &SlurmJobEntry) -> Result<(), Error> {
-    let target_path = archive_path.join(format!("job.{}_{}", &slurm_job_entry.jobid, &slurm_job_entry.filename));
-    match copy(&slurm_job_entry.path, &target_path) {
-        Ok(bytes) => debug!("copied {} bytes from {:?} to {:?}", bytes, &slurm_job_entry.path, &target_path),
-        Err(e) => {
-            error!("Copy of {:?} to {:?} failed: {:?}", &slurm_job_entry.path, &target_path, e);
-            return Err(e);
+
+    // We wait for each file to be present
+    let ten_millis = Duration::from_millis(10);
+    for filename in vec!["script", "environment"] {
+
+        let fpath = slurm_job_entry.path.join("script");
+        let mut iters = 100;
+        while !Path::exists(&fpath) && iters > 0 { 
+            debug!("Waiting for {:?}", fpath);
+            sleep(ten_millis);
+            if !Path::exists(&slurm_job_entry.path) {
+                error!("Job directory {:?} no longer exists", &slurm_job_entry.path);
+                panic!("path not found");
+            }
+            iters -= 1;
         }
+        if iters == 0 {
+            warn!("Cannot make copy of {:?}", fpath);
+            continue;
+        }
+
+        let target_path = archive_path.join(format!("job.{}_{}", &slurm_job_entry.jobid, &filename));
+        match copy(&fpath, &target_path) {
+            Ok(bytes) => info!("copied {} bytes from {:?} to {:?}", bytes, &fpath, &target_path),
+            Err(e) => {
+                error!("Copy of {:?} to {:?} failed: {:?}", &slurm_job_entry.path, &target_path, e);
+                return Err(e);
+            }
+        };
     };
+    
     Ok(())
 }
 
@@ -95,8 +114,8 @@ fn check_and_queue(s: &Sender<SlurmJobEntry>, event: DebouncedEvent) -> Result<(
     debug!("Event received: {:?}", event);
     match event {
         DebouncedEvent::Create(path) | DebouncedEvent::Write(path) => {
-            if let Some((jobid, job_filename)) = is_job_path(&path) {
-                let e = SlurmJobEntry::new(&path, jobid, job_filename);
+            if let Some((jobid, dirname)) = is_job_path(&path) {
+                let e = SlurmJobEntry::new(&path, jobid);
                 s.send(e).unwrap();
             };
         }
@@ -114,7 +133,7 @@ pub fn monitor(base: &Path, hash: u8, s: &Sender<SlurmJobEntry>) -> notify::Resu
     let path = base.join(format!("hash.{}", hash));
 
     // TODO: check the path exists!
-    watcher.watch(&path, RecursiveMode::Recursive)?;
+    watcher.watch(&path, RecursiveMode::NonRecursive);
     loop {
         match rx.recv() {
             Ok(event) => check_and_queue(s, event)?,
