4b3c56314bd4bb3f72aeb3d045b3daf25912a95e
Andy Georges <itkovian@gmail.com>
2019-05-27 10:25:37 +0200

    Remove duplicate apply

--- src/main.rs
+++ src/main.rs
@@ -54,7 +54,7 @@ fn setup_logging(level_filter: log::LevelFilter, logfile: Option<&str>) -> Resul
         .level(level_filter);
 
     match logfile {
-        Some(filename) => base_config.chain(fern::log_file(filename).unwrap()),
+        Some(filename) => base_config.chain(fern::log_file("output.log").unwrap()),
         None => base_config.chain(std::io::stdout())
     }.apply()
 }
