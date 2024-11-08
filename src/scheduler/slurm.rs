2c24caa2061743e2970726c5b4d4b1adecbe2f9b
Andy Georges <itkovian@gmail.com>
2024-11-08 16:28:43 +0100

    fix: remove unused import

--- src/scheduler/slurm.rs
+++ src/scheduler/slurm.rs
@@ -19,7 +19,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
-use log::{debug, info};
+use log::debug;
 use notify::event::{CreateKind, Event, EventKind};
 use regex::Regex;
 use std::collections::HashMap;
